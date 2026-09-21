5aa77950840ddbc94824a1da0066262f7007a890
inari-1234 <shouri95@yahoo.co.jp>
2026-09-21 09:35:50 +0900

    Set Build 42 application metadata

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const APP_VERSION='1.17.0';
-const APP_BUILD='2026.09.21-41';
+const APP_BUILD='2026.09.21-42';
 const KEY='dabistaFarmV1';
 const errors=[];
 let updateState={lastCheck:null,status:'not-checked',remote:null};
